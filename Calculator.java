fdce3eb8766ca8f98218cbab03362e331369aa38
ashishbehera29 <87660082+ashishbehera29@users.noreply.github.com>
2024-09-15 17:13:21 +0530

    compile time polymerphism

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public String add(String a, String b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.add(5, 10));           
+        System.out.println(calc.add(5.5, 10.5));       
+        System.out.println(calc.add("Hello", " World")); 
+    }
+}
